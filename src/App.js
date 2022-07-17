be12c1e13ad9753d633f9dd8ad6e94598270ed0d
Akshat Gadodia <akshatgadodia@gmail.com>
2022-07-17 15:55:51 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react'
+import Display from './Display';
+import Form from './Form';
 
 function App() {
+  const [data,setData] = useState([]);
+  const addData = (newData) =>{
+    setData((prev)=>{
+      return [...prev,newData]
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form className='' addData={addData}/>
+      <Display className='' data={data} />
     </div>
   );
 }
